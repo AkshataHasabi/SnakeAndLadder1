b358b47cf38e6c6d04e5f3c22ef695b314b49888
AkshataHasabi <akshuh818@gmail.com>
2021-12-26 17:16:27 +0530

    ADDED:used random function

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,10 +3,7 @@ package com.bridgelab.snakeladder;
 public class SnakeLadder {
 
 	public static void main(String[] args) {
-		int position=0;
-		int player1=0;
-		System.out.println("Start the game");
-
+		double randomVar = (Math.floor(Math.random() * 10) % 6) + 1;
+		System.out.println("Dice rolled value: " + randomVar);
 	}
-
 }
