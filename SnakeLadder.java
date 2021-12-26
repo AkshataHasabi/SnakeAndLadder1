b9ede7f1736cd5fd2c734e7406d11e28cfbf80ad
AkshataHasabi <akshuh818@gmail.com>
2021-12-26 17:08:49 +0530

    ADDED:single player position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,6 +3,8 @@ package com.bridgelab.snakeladder;
 public class SnakeLadder {
 
 	public static void main(String[] args) {
+		int position=0;
+		int player1=0;
 		System.out.println("Start the game");
 
 	}
