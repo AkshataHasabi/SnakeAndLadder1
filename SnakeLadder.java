01706d266ea705f40b1bce112e7e68257b535746
AkshataHasabi <akshuh818@gmail.com>
2021-12-26 17:30:12 +0530

    ADDED:player will reach position 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,29 +1,32 @@
 package com.bridgelab.snakeladder;
 
 public class SnakeLadder {
-	public static final int NoPlayOption=0;
-	public static final int ladderOption=1;
-	public static final int SnakeOption=2;
-	public static int PlayerOne=1;
-	public static void main(String[] args){
-		//variable
-		double position = 0;
-		double diceRoll = (Math.floor(Math.random() * 10) % 6) + 1;
-            	System.out.println("Dice rolled value: " + diceRoll);
-            	int option = (int) Math.floor(Math.random() * 10) % 3;
-            	switch (option) {
-            	case NoPlayOption:
-            		position = position + 0;
-            		break;
-            	case ladderOption:
-            		position = position + diceRoll;
-            		break;
-            	case SnakeOption:
-            		position = position - diceRoll;
-            		if(position < 0){
-            			position = 0;
-            		}
-            		break;
-            	}
+	public static final int NoPlayOption = 0;
+    public static final int ladderOption = 1;
+    public static final int SnakeOption = 2;
+    public static int PlayerOne = 1;
+    
+    public static void main(String[] args){
+    		double position = 0;
+    		while (position < 100){
+    			double diceRoll = (Math.floor(Math.random() * 10) % 6) + 1;
+    			System.out.println("Dice rolled value: " + diceRoll);
+    			int option = (int) Math.floor(Math.random() * 10) % 3;
+    			switch (option) {
+            		case NoPlayOption:
+            				position = position + 0;
+            				break;
+            		case ladderOption:
+            				position = position + diceRoll;
+            				break;
+            		case SnakeOption:
+                    		position = position - diceRoll;
+                    		if(position < 0){
+                            		position = 0;
+                    		}
+                    		break;
+    			}
+    		}
+    		System.out.println("Player Wins with Position :" + position);
 		}
 }
